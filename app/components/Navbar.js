ae1d3b471a97e89855b8646a91ee961bffccea45
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2021-12-13 14:32:38 +0100

    add navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <div className={styles.navcontainer}>
+      <p>oui</p>
+      <p>oui</p>
+      <p>oui</p>
+    </div>
+  );
+}
